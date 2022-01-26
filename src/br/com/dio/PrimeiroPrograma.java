f960ad2c4edb21d8363badfd6f2266c754957890
Fernando Kain <fernandomanga@hotmail.com>
2022-01-26 01:17:14 -0300

    Getters and Setters, toString, equals e HashCode added.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
@@ -17,5 +19,52 @@ public class PrimeiroPrograma {
 */
 
 
+    }
+
+    class Livro{
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Livro livro = (Livro) o;
+            return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nome, numPaginas);
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
     }
 }
